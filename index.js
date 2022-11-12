bc25c5b1ba9758cb86fecc6230d7cd19c95369a0
archifix <107472693+archifix@users.noreply.github.com>
2022-11-12 12:37:34 +0300

    Update

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
+require('dotenv').config('./.env')
 const express = require('express')
 
+
+const PORT = process.env.PORT
+
 const app = express()
+
+
+const start = async () => {
+  try {
+  app.listen(PORT, () => console.log(`Server OK on:${PORT}`))
+} catch(e) {
+  console.log(e)
+}
+}
+
+start()
